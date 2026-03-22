6bcabc77eabc9fd3bb798c59b97d1a7c1a5f8ae0
Damir Vafin <damirvafinnailevich@gmail.com>
2026-03-22 23:00:38 +0300

    Добавить тесты для ErrorHandler

--- src/test/java/ru/yandex/practicum/filmorate/handler/ErrorHandlerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/handler/ErrorHandlerTest.java
@@ -1,4 +1,48 @@
 package ru.yandex.practicum.filmorate.handler;
 
-public class ErrorHandlerTest {
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import ru.yandex.practicum.filmorate.controller.FilmController;
+import ru.yandex.practicum.filmorate.exception.NotFoundException;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
+import ru.yandex.practicum.filmorate.service.FilmService;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(FilmController.class)
+class ErrorHandlerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private FilmService filmService;
+
+    @Test
+    void handleNotFoundException_ShouldReturn404() throws Exception {
+        when(filmService.getFilmById(999L))
+                .thenThrow(new NotFoundException("Фильм с id - 999 не найден"));
+
+        mockMvc.perform(get("/films/999"))
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.error").value("Ресурс не найден"))
+                .andExpect(jsonPath("$.description").value("Фильм с id - 999 не найден"));
+    }
+
+    @Test
+    void handleValidationException_ShouldReturn400() throws Exception {
+        when(filmService.getMostLikedFilms(-1, null, null))
+                .thenThrow(new ValidationException("Укажите положительный параметр count"));
+
+        mockMvc.perform(get("/films/popular?count=-1"))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.error").value("Ошибка валидации данных"))
+                .andExpect(jsonPath("$.description").value("Укажите положительный параметр count"));
+    }
 }
