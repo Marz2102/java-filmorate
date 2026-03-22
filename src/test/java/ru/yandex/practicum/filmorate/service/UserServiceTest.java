039949f661b4a26c643fd5dc0dfdaa7ebc4029a0
Damir Vafin <damirvafinnailevich@gmail.com>
2026-03-22 18:09:11 +0300

    test: добавить мок для EventStorage в UserServiceTest

--- src/test/java/ru/yandex/practicum/filmorate/service/UserServiceTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/service/UserServiceTest.java
@@ -13,6 +13,7 @@ import ru.yandex.practicum.filmorate.dto.user.UserUpdateDto;
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.storage.event.EventStorage;
 import ru.yandex.practicum.filmorate.storage.film.FilmStorage;
 import ru.yandex.practicum.filmorate.storage.user.UserStorage;
 
@@ -23,6 +24,7 @@ import java.util.Optional;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -33,6 +35,9 @@ class UserServiceTest {
 
     @Mock
     private FilmStorage filmStorage;
+
+    @Mock
+    private EventStorage eventStorage;
 
     @InjectMocks
     private UserService userService;
@@ -329,5 +334,4 @@ class UserServiceTest {
         verify(userStorage).findById(999L);
         verify(eventStorage, never()).getEvents(anyLong());
     }
-
 }
