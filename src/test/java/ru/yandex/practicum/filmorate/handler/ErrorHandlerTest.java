ef6b734b59bb46594b6bf501fe713e523c256b4d
Damir Vafin <damirvafinnailevich@gmail.com>
2026-03-23 00:52:58 +0300

    test: добавить тесты для InternalServerError и UnsupportedMediaType в ErrorHandler

--- src/test/java/ru/yandex/practicum/filmorate/handler/ErrorHandlerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/handler/ErrorHandlerTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import ru.yandex.practicum.filmorate.controller.FilmController;
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
@@ -12,6 +13,7 @@ import ru.yandex.practicum.filmorate.service.FilmService;
 
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -44,5 +46,25 @@ class ErrorHandlerTest {
                 .andExpect(status().isBadRequest())
                 .andExpect(jsonPath("$.error").value("Ошибка валидации данных"))
                 .andExpect(jsonPath("$.description").value("Укажите положительный параметр count"));
+    }
+
+    @Test
+    void handleInternalServerException_ShouldReturn500() throws Exception {
+        when(filmService.getFilmById(1L))
+                .thenThrow(new RuntimeException("Unexpected database error"));
+
+        mockMvc.perform(get("/films/1"))
+                .andExpect(status().isInternalServerError())
+                .andExpect(jsonPath("$.error").value("Ошибка сервера"))
+                .andExpect(jsonPath("$.description").value("Unexpected database error"));
+    }
+
+    @Test
+    void handleUnsupportedMediaTypeException_ShouldReturn415() throws Exception {
+        mockMvc.perform(post("/films")
+                        .contentType(MediaType.APPLICATION_XML)
+                        .content("<film><name>Test</name></film>"))
+                .andExpect(status().isUnsupportedMediaType())
+                .andExpect(jsonPath("$.error").value("Неподдерживаемый тип тела запроса"));
     }
 }
