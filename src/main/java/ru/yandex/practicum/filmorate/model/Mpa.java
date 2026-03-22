5bc74679220ef10e8893452eb175f71132d22a2d
Damir Vafin <damirvafinnailevich@gmail.com>
2026-03-23 01:04:53 +0300

    feat: добавить equals и hashCode в Mpa на основе id

--- src/main/java/ru/yandex/practicum/filmorate/model/Mpa.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Mpa.java
@@ -4,10 +4,25 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.Objects;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class Mpa {
     private Long id;
     private String name;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Mpa mpa = (Mpa) o;
+        return Objects.equals(id, mpa.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
 }
